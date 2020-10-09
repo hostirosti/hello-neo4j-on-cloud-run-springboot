aa64b026ec7a4d0241b4fe6f02f148b46e13510c
Robert Kubis <hostirosti@users.noreply.github.com>
2020-10-09 15:31:00 -0600

    Update precreate.sh

--- .gcloud/precreate.sh
+++ .gcloud/precreate.sh
@@ -8,12 +8,12 @@ declare sa=$NUM-compute@developer.gserviceaccount.com
 
 gcloud secrets add-iam-policy-binding neo4j-aura-uri \
   --member=serviceAccount:$sa \
-  --role=roles/secretmanager.secretAccessor &> /dev/null
+  --role=roles/secretmanager.secretAccessor --project=$project &> /dev/null
   
 gcloud secrets add-iam-policy-binding neo4j-aura-user \
   --member=serviceAccount:$sa \
-  --role=roles/secretmanager.secretAccessor &> /dev/null
+  --role=roles/secretmanager.secretAccessor --project=$project &> /dev/null
   
 gcloud secrets add-iam-policy-binding neo4j-aura-password \
   --member=serviceAccount:$sa \
-  --role=roles/secretmanager.secretAccessor &> /dev/null
+  --role=roles/secretmanager.secretAccessor --project=$project &> /dev/null
